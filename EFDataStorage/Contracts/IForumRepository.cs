4268bed8b14d3ba1dd022de1cbf0dcd26fd8c5cb
nikxsh <shinde.nikhilesh90@gmail.com>
2017-04-05 22:11:49 +0530

    Adding missing file

---
+++ EFDataStorage/Contracts/IForumRepository.cs
@@ -0,0 +1,16 @@
+﻿using EFDataStorage.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace EFDataStorage.Contracts
+{
+    public interface IForumRepository
+    {
+        IEnumerable<Reply> GetRepliesByTopic(Guid Id);
+        IEnumerable<Topic> GetTopics();      
+        Topic GetTopicById(Guid TopicId);
+        void SubmitTopic(Topic Topic);
+        void SubmitReply(Reply Reply);
+       
+    }
+}
---
+++ EFDataStorage/Repositories/ForumRepository.cs
@@ -0,0 +1,88 @@
+﻿using EFDataStorage.Contracts;
+using EFDataStorage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFDataStorage.Repositories
+{
+    public class ForumRepository : IForumRepository
+    {
+        public IEnumerable<Reply> GetRepliesByTopic(Guid Id)
+        {
+            try
+            {
+                using (var context = new ForumContext())
+                {
+                    return context.Reply.Where(x => x.TopicId == Id).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IEnumerable<Topic> GetTopics()
+        {
+            try
+            {
+                using (var context = new ForumContext())
+                {
+                    return context.Topics.OrderByDescending(y => y.Created).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Topic GetTopicById(Guid TopicId)
+        {
+            try
+            {
+                using (var context = new ForumContext())
+                {
+                    return context.Topics.FirstOrDefault(x => x.Id == TopicId);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SubmitReply(Reply Reply)
+        {
+            try
+            {
+                using (var context = new ForumContext())
+                {
+                    context.Reply.Add(Reply);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SubmitTopic(Topic Topic)
+        {
+            try
+            {
+                using (var context = new ForumContext())
+                {
+                    context.Topics.Add(Topic);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }      
+    }
+}
