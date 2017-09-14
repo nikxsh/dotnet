4afc5f22dfbc4d65be6558a24ca455bc4e7486a4
nikxsh <shinde.nikhilesh90@gmail.com>
2017-09-14 15:07:00 +0530

    Style and other changes

--- Angular-CLI/src/app/app.component.ts
+++ Angular-CLI/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular WTF!';
+  title = 'Angular world!';
+  
+   employeeList = [
+     { id: 1001, Name: 'Nikhilesh Shinde', Dob: new Date(1990,2,14).toDateString()},
+     { id: 1002, Name: 'John Snow', Dob: new Date(1990,4,14).toDateString()},
+     { id: 1003, Name: 'Arya Stark', Dob: new Date(1990,7,14).toDateString()},
+     { id: 1004, Name: 'Emma Watson', Dob: new Date(1990,11,14).toDateString()}    
+    ]
 }
