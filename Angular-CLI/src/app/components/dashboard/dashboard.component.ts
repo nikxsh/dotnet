284fd35f956b520b7007a8387d0ecff5b4d4a090
nikxsh <shinde.nikhilesh90@gmail.com>
2017-09-21 13:10:05 +0530

    final framework (Ready to use)

--- Angular-CLI/src/app/components/dashboard/dashboard.component.ts
+++ Angular-CLI/src/app/components/dashboard/dashboard.component.ts
@@ -6,12 +6,37 @@ import { Component, OnInit, ElementRef } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  currentDateTime: Date;
 
-  constructor(private elementRef: ElementRef) { };
+  constructor(private elementRef: ElementRef) { 
+    this.utcTime();
+  };
 
   ngOnInit() {
   }
-  
+
   ngAfterViewInit() {
+    this.addScript("assets/scripts/site.js");
+    this.addCss("assets/css/metis-menu.css");
   }
+
+  addScript(link) {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = link;
+    this.elementRef.nativeElement.appendChild(s);
+  }
+
+  addCss(link) {
+    var s = document.createElement("link");
+    s.rel = "stylesheet";
+    s.href = link;
+    this.elementRef.nativeElement.appendChild(s);
+  }
+
+  private utcTime() {
+    setInterval(() => {
+        this.currentDateTime = new Date();
+    }, 1000);
+}
 }
