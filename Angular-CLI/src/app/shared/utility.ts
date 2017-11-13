6ef7ab57d592cb6f8f785511909c5db84edb3a89
nikxsh <shinde.nikhilesh90@gmail.com>
2017-11-13 15:47:21 +0530

    Cleaning

--- Angular-CLI/src/app/shared/utility.ts
+++
@@ -1,8 +0,0 @@
-import { DatePipe } from "@angular/common";
-
-export namespace Utilty {
-    export function FormatDate(source: string): string {
-        var formatter =  new DatePipe('en-US');
-        return formatter.transform(source,'short');
-    }
-}
