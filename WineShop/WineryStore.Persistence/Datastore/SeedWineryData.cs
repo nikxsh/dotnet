3a9326e2f24d1a54df0dfab1a7f7c29f73f41da0
nikxsh <shinde.nikhilesh90@gmail.com>
2020-01-20 19:18:29 +0530

    Updated Seed process

---
+++ WineShop/WineryStore.Persistence/Datastore/SeedWineryData.cs
@@ -0,0 +1,53 @@
+﻿using Shared;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using static WineryStore.Persistence.Datastore.WineryContext;
+
+namespace WineryStore.Persistence.Datastore
+{
+	class WineryEqualityComparer : IEqualityComparer<Winery>
+	{
+		public bool Equals([AllowNull] Winery x, [AllowNull] Winery y)
+		{
+			return x.Name == y.Name;
+		}
+
+		public int GetHashCode([DisallowNull] Winery obj)
+		{
+			return obj.Name.GetHashCode();
+		}
+	}
+
+	class WineEqualityComparer : IEqualityComparer<Wine>
+	{
+		public bool Equals([AllowNull] Wine x, [AllowNull] Wine y)
+		{
+			return x.Name == y.Name;
+		}
+
+		public int GetHashCode([DisallowNull] Wine obj)
+		{
+			return obj.Name.GetHashCode();
+		}
+	}
+
+	public class SeedWineryData
+	{
+		public static HashSet<Winery> GetWineries()
+		{
+			return new 
+				FileHandler()
+				.ReadJsonData<Winery[]>(@"..\..\Files\wineries.json")
+				.ToHashSet();
+		}
+
+		public static HashSet<Wine> GetWines()
+		{
+			return new 
+				FileHandler()
+				.ReadJsonData<Wine[]>(@"..\..\Files\wines.json")
+				.ToHashSet();
+		}
+	}
+}
