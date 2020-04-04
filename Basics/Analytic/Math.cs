f7fe0a84d9e04d099343b090dad84193b246235a
nikxsh <shinde.nikhilesh90@gmail.com>
2020-04-04 20:50:16 +0530

    Problem Solving

--- Basics/Analytic/Math.cs
+++ Basics/Analytic/Math.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Analytic
 {
 	public class Math
 	{
+
+		public Math()
+		{
+
+			//PatternMatching1("xyz", "abcd"); //0
+			//PatternMatching1("abcd", "abcd"); //1
+			//PatternMatching1("12abcdop", "abcd"); //2
+			//PatternMatching1("ab123cd", "*abcd"); //0
+			//PatternMatching1("ab123cd", "abcd*"); //0
+			//PatternMatching1("ab123cd", "a*bcd"); //0
+			//PatternMatching1("xab123cdyzxab987cd", "ab*cd"); //2
+			//PatternMatching1("xyzab123cd", "ab*cd"); //4
+			//PatternMatching1("xyzab123cxyzab678cd", "ab*cd"); //13
+
+			Problem2(new List<int> { 1, 1, 1, 2 });
+			Problem2(new List<int> { 1, 2, 1, 1, 2 });
+			Problem2(new List<int> { 100, 2, 4, 2, 3, 2, 100 });
+		}
+
 		/// <summary>
-		/// 1. Take min value from input array
+		/// 1. Take Max value from input array
 		/// 2. Divide by 2 and get floor value i.e. 3/2 = 1.5 ~ 2
 		/// 3. Replace this value with previous
 		/// 4. Repeat this process K times
@@ -14,14 +34,41 @@ namespace Analytic
 		/// </summary>
 		public void Problem1(int[] input, int k)
 		{
-			for(int i = 0; i < k; i++)
+			for (int i = 0; i < k; i++)
 			{
-				var min = input.Min();
-				var divideBy2 = decimal.Round((min / 2m), MidpointRounding.AwayFromZero);
+				var min = input.Max();
+				var divideBy2 = decimal.Ceiling((min / 2m));
 				var minIndex = Array.IndexOf(input, min);
-				input[minIndex] = (int) divideBy2;
+				input[minIndex] = (int)divideBy2;
 			}
 			Console.WriteLine(input.Sum());
+		}
+
+		public void Problem2(List<int> input)
+		{
+			List<int> subSequence = new List<int>();
+			for (int i = 0; i < input.Count; i++)
+			{
+				IEnumerable<int> nextRange = input.Skip(i + 1);
+				foreach (var item in nextRange)
+				{
+					if (input[i] == item)
+					{
+						subSequence.Add(input[i]);
+						break;
+					}
+				}
+			}
+
+			foreach (var item in input)
+				Console.Write($"{item} ");
+
+			Console.Write(" >> ");
+
+			foreach (var item in subSequence)
+				Console.Write($"{item} ");
+
+			Console.WriteLine();
 		}
 	}
 }
--- Basics/Analytic/Program.cs
+++ Basics/Analytic/Program.cs
@@ -6,16 +6,8 @@ namespace Analytic
 	{
 		static void Main(string[] args)
 		{
-			var matching = new Matching();
-			matching.PatternMatching1("xyz", "abcd"); //0
-			matching.PatternMatching1("abcd", "abcd"); //1
-			matching.PatternMatching1("12abcdop", "abcd"); //2
-			matching.PatternMatching1("ab123cd", "*abcd"); //0
-			matching.PatternMatching1("ab123cd", "abcd*"); //0
-			matching.PatternMatching1("ab123cd", "a*bcd"); //0
-			matching.PatternMatching1("xab123cdyzxab987cd", "ab*cd"); //2
-			matching.PatternMatching1("xyzab123cd", "ab*cd"); //4
-			matching.PatternMatching1("xyzab123cxyzab678cd", "ab*cd"); //13
+			//new Matching();
+			new Math();
 
 			Console.ReadKey();
 		}
